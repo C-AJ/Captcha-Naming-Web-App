b82e6b3f0b5593ad547cd60c7a7701a28bdbb6b5
Joshua <student.joshuaestrada@gmail.com>
2023-10-08 15:49:44 -0700

    added settings for deployment

--- CaptchaSite/CaptchaSite/settings.py
+++ CaptchaSite/CaptchaSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vn7zb3mfc08ibz3ln0av&^0vya5o=^%6%&kz9r2iuie)ty#z=d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
