85b554882f372f2161a6259d98b39ecd67c9fc0a
Joshua <student.joshuaestrada@gmail.com>
2023-10-08 15:57:46 -0700

    updated settings

--- CaptchaSite/CaptchaSite/settings.py
+++ CaptchaSite/CaptchaSite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vn7zb3mfc08ibz3ln0av&^0vya5o=^%6%&kz9r2iuie)ty#z=d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app']
 
